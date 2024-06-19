09aede67315c6eec4f2c76bbacf34259610a867f
JaiPrakashPingalExternLabs <jaiprakashpingal@externlabs.com>
2024-06-19 16:08:28 +0530

    feat: imp. images constants and Scale

---
+++ src/theme/ImageConstants.tsx
@@ -0,0 +1,10 @@
+export const SearchIcon = require('../assets/Icons/searchIcon.png');
+export const UnselectIcon = require('../assets/Icons/unselected-icon.png');
+export const SelectedIcon = require('../assets/Icons/selected-icon.png');
+export const UPDROPDOWNICON = require('../assets/Icons/updrop.png');
+export const DOWNDROPDOWNICON = require('../assets/Icons/downdrop.png');
+// export const ATWORKCANCELICON = require('../assets/Icons/ArtworkCancel.png');
+// export const ATWORKCANCELICON = require('../assets/Icons/ArtworkCancel.png');
+// export const ATWORKCANCELBLURICON = require('../assets/Icons/atWorkCancelBlur.png');
+// export const ATWORKCANCELICON = require('../assets/Icons/updrop.png');
+// export const DOWNDROPDOWNICON = require('../assets/Icons/downdrop.png');
